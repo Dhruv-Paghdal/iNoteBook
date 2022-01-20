1afcaf237ac1c60b18e77aeba209218ca70aef91
Dhruv-Paghdal <85886757+Dhruv-Paghdal@users.noreply.github.com>
2022-01-20 13:01:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let favBtn=document.getElementById("favBtn");
 let favouriteSection=document.getElementById("favouriteSec"); 
 
 // Setting Up the data for NoteBook
-let titleData=["iNoteBook<br>Tagline will go here","Table Of Content","Title 1","Title 2","Title 3","Title 4","Title 5","Title 6","Title 7","Title 8","Title 9","Title 10"]
+let titleData=["iNoteBook<br>Tagline will go here","Table Of Content","Title 1","Title 2","Title 3","Title 4","Title 5","Title 6","Title 7","Title 8","Title 9","Title 10"];
 let contentData=["<font color='white'>EDITION 2022</font>",
                 "<table class='table h-auto text-center'><thead><tr><th scope='col'>*</th><th scope='col'>Title</th><th scope='col'>Page Number</th></tr></thead><tbody><tr><th scope='row'>1</th><td>Title 1</td><td>3</td></tr><tr><th scope='row'>2</th><td>Title 2</td><td>4</td></tr><tr><th scope='row'>3</th><td>Title 3</td><td>5</td></tr><tr><th scope='row'>4</th><td>Title 4</td><td>6</td></tr><tr><th scope='row'>5</th><td>Title 5</td><td>7</td></tr><tr><th scope='row'>6</th><td>Title 6</td><td>8</td></tr><tr><th scope='row'>7</th><td>Title 7</td><td>9</td></tr><tr><th scope='row'>8</th><td>Title 8</td><td>10</td></tr><tr><th scope='row'>9</th><td>Title 9</td><td>11</td></tr><tr><th scope='row'>10</th><td>Title 10</td><td>12</td></tr></tbody></table>",
                 "Within this report is information on how Java Script is used on the Internet. This report will contain information that will help you learn about Java Script and its uses on the Internet. It mentions what Java Script is, where it originated and what some of its uses are.JavaScript is a loosely typed scripting language that resembles the programming language C. It is designed to be an extension to HTML and is usually included within HTML scripts. Java Script is object-oriented and has block-structuring features. The main feature of Java Script is that it lets you build interactive web pages.",
@@ -24,8 +24,8 @@ let contentData=["<font color='white'>EDITION 2022</font>",
                 "JavaScript is not a programming language in strict sense. Instead, it is a scripting language because it uses the browser to do the dirty work. If you command an image to be replaced by another one, JavaScript tells the browser to go do it. Because the browser actually does the work, you only need to pull some strings by writing some relatively easy lines of code. That’s what makes JavaScript an easy language to start with.",
                 "But don’t be fooled by some beginner’s luck: JavaScript can be pretty difficult, too. First of all, despite its simple appearance it is a full fledged programming language: it is possible to write quite complex programs in JavaScript. This is rarely necessary when dealing with web pages, but it is possible. This means that there are some complex programming structures that you’ll only understand after protracted studies.",
                 "Even with all these improvements, there is still a catch: JavaScript is flaky. Not the language itself but the environment it is implemented in. You do not know what computer is on the receiving end of your web page, you do not know how busy the computer is with other things, and you do not know if some other JavaScript in another tab of the browser is grinding things down to a halt. Until browsers begin having different processing resources for different tabs and windows (also known as threads), this will always remain an issue. Multiple threading is made available to a certain degree by a new HTML5 feature called Web workers, and this has reasonable browser support."]
-let footerData=["SLOGAN TEXT WILL GO HERE<br>LOREM IPSUM DOLOR SITE AMET","Table Of Content Footer","Footer 1","Footer 2","Footer 3","Footer 4","Footer 5","Footer 6","Footer 7","Footer 8","Footer 9","Footer 10"]
-let favourite=[0,0,0,0,0,0,0,0,0,0,0,0]
+let footerData=["SLOGAN TEXT WILL GO HERE<br>LOREM IPSUM DOLOR SITE AMET","Table Of Content Footer","Footer 1","Footer 2","Footer 3","Footer 4","Footer 5","Footer 6","Footer 7","Footer 8","Footer 9","Footer 10"];
+let favourite=[0,0,0,0,0,0,0,0,0,0,0,0];
 
 let count=1;
 let btns=new Array();
@@ -38,7 +38,7 @@ let addFav=()=>{
     else{
         favourite[count-1]=1
         btns.push(`<button class="btn btn-primary mx-2" onclick="customPage(this.value)" value="${count}">${count}</button>`);
-        favouriteSection.innerHTML=favouriteSection.innerHTML+btns[(btns.length)-1]
+        favouriteSection.innerHTML=favouriteSection.innerHTML+btns[(btns.length)-1];
         alert("Added To Favourite");
     }
 }
@@ -68,22 +68,23 @@ let checkCount=()=>{
 
 // Function for setting content on a particular page
 let setData=(data)=>{
-    title.innerHTML=titleData[data]
-    content.innerHTML=contentData[data]
-    footer.innerHTML=footerData[data]
+    title.innerHTML=titleData[data];
+    content.innerHTML=contentData[data];
+    footer.innerHTML=footerData[data];
 }
 
 // Function for getting desired page number
 let customPage=(num)=>{
-    count=parseInt(num);
-    if(count<=0 || count>12){
-        alert('Enter a value between 1-12')
+    let temp=parseInt(num);
+    if(temp<=0 || temp>12){
+        alert('Enter a value between 1-12');
     }
     else{
+        count=temp;
         checkCount();
-        title.innerHTML=titleData[count-1]
-        content.innerHTML=contentData[count-1]
-        footer.innerHTML=footerData[count-1]
+        title.innerHTML=titleData[count-1];
+        content.innerHTML=contentData[count-1];
+        footer.innerHTML=footerData[count-1];
         pageNumber.innerHTML=`Page No. ${count}`;
     }
 }
@@ -109,7 +110,7 @@ let prev=()=>{
 page.style.backgroundImage="url('b1.png')";
 content.style.height="60vh";
 
-title.innerHTML=titleData[0]
-content.innerHTML=contentData[0]
-footer.innerHTML=footerData[0]
+title.innerHTML=titleData[0];
+content.innerHTML=contentData[0];
+footer.innerHTML=footerData[0];
 pageNumber.innerHTML=`Page No. ${count}`;
